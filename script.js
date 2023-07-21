0e6a97f1c08f1472ab9be6c24320e723efd99140
axelanceweb <140127229+axelanceweb@users.noreply.github.com>
2023-07-21 20:00:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 const input = document.querySelector('input');
-document.addEventListener('keydown', function(e) {
-  if(e.keyCode === 13 && input.value){
-    var node = document.createElement("DIV");  
-    node.style.color = "#888";
-    node.style.fontSize = "12px";
-    node.style.paddingLeft = '5px';
-    
-    if(input.value === 'help'){
-      const para = document.createElement("p");
-      para.style.display = 'block';
-const text = document.createTextNode('Supported commands: about, experience, education, projects, skills, contact');
-para.appendChild(text);
-            document.querySelector(".commands").appendChild(para);
-    }
 
-if(input.value === 'about')
-{
-const para = document.createElement("p");
-      para.style.display = 'block';
-const text = document.createTextNode('21 yaşında Sosyal Medya ile ilgilenen, Boş zamanlarında kodlama yapan bir yazılımcı.');
-para.appendChild(text);
-            document.querySelector(".commands").appendChild(para);
+        document.addEventListener('keydown', function (e) {
+            if (e.keyCode === 13 && input.value) {
+                var node = document.createElement('DIV');
+                node.style.color = '#888';
+                node.style.fontSize = '12px';
+                node.style.paddingLeft = '5px';
 
-}
+                if (input.value === 'help') {
+                    const para = createParagraph('Supported commands: about, experience, education, projects, skills, contact');
+                    document.querySelector('.commands').appendChild(para);
+                } else if (input.value === 'about') {
+                    const para = createParagraph('21 yaşında Sosyal Medya ile ilgilenen, Boş zamanlarında kodlama yapan bir yazılımcı.');
+                    document.querySelector('.commands').appendChild(para);
+                } else {
+                    const para = createParagraph('→ command not found: ' + input.value);
+                    document.querySelector('.commands').appendChild(para);
+                }
 
-else{
-      const para = document.createElement("p");
-      para.style.display = 'block';
-const text = document.createTextNode('→ command not found:' + input.value);
-para.appendChild(text);
-            document.querySelector(".commands").appendChild(para);
-    }
-    input.value = "";
-  }
-});
+                input.value = '';
+            }
+        });
 
-function f(){ document.querySelector('input').focus();
-}
+        function createParagraph(text) {
+            const para = document.createElement('p');
+            para.style.display = 'block';
+            const paraText = document.createTextNode(text);
+            para.appendChild(paraText);
+            return para;
+        }
